44a5d1b6df9a807765c733595ea1ff2472ff2910
leoagustino <gunyearsofficial@gmail.com>
2021-12-08 13:44:29 +0700

    login

--- app/src/main/java/com/example/cahpintar/MainActivity.java
+++ app/src/main/java/com/example/cahpintar/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cahpintar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), login.class));
+            }
+        });
     }
-}
+
+    }
