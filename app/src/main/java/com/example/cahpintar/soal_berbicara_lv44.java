23f6d7efc318c8f86ed087fd867d27cf23696494
syfr456 <sekarsyafira225@gmail.com>
2022-01-16 11:17:32 +0700

    update soal lv4

---
+++ app/src/main/java/com/example/cahpintar/soal_berbicara_lv44.java
@@ -0,0 +1,14 @@
+package com.example.cahpintar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class soal_berbicara_lv44 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soal_berbicara_lv44);
+    }
+}
