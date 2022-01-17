cfcbcfbe5c8fc0c737f27a81c5d1911f3eec1daf
leoagustino <gunyearsofficial@gmail.com>
2022-01-17 13:57:22 +0700

    Quiz

--- app/src/main/java/com/example/cahpintar/MainActivity.java
+++ app/src/main/java/com/example/cahpintar/MainActivity.java
@@ -4,30 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Handler h = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnStart = findViewById((R.id.start));
-        btnStart.setOnClickListener(new View.OnClickListener() {
+        h.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, TestIq1.class));
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), login.class));
+                finish();
             }
-        });
+        }, 5000L);
 
-        Button btnStart2 = findViewById((R.id.start2));
-        btnStart2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, testiq2.class));
-            }
-        });
+
     }
 }
