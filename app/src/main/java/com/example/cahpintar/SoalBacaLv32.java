120b6bcf0e1732320d9ac5680bd97938d760476a
leoagustino <gunyearsofficial@gmail.com>
2022-01-17 13:08:50 +0700

    quiz

---
+++ app/src/main/java/com/example/cahpintar/SoalBacaLv32.java
@@ -0,0 +1,14 @@
+package com.example.cahpintar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoalBacaLv32 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soal_baca_lv32);
+    }
+}
