64fd0e28ccf858390e74ad0f973f75e7c9b8f516
syfr456 <sekarsyafira225@gmail.com>
2022-01-21 14:48:30 +0700

    update lv5

---
+++ app/src/main/java/com/example/cahpintar/ssoal_membaca_lv55.java
@@ -0,0 +1,14 @@
+package com.example.cahpintar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ssoal_membaca_lv55 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soal_membaca_lv55);
+    }
+}
